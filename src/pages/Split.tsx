d9387e0c32f95f208be842aed68f93683e9c72d5
Thaya <selvathayalan.r@gmail.com>
2023-06-27 08:48:50 +0530

    Added conditional based outlest to show expense list in split index page as default.

--- src/pages/Split.tsx
+++ src/pages/Split.tsx
@@ -1,13 +1,17 @@
 import '../styles/Split.scss'
 import { useSelector, TypedUseSelectorHook } from 'react-redux';
 import { Split as SplitModel } from "../models/Split";
-import { Outlet, useParams } from 'react-router-dom';
+import { useOutlet, useParams } from 'react-router-dom';
 import { RootState } from '../store/store';
+import { Expense } from '../models/Expense';
+import ExpenseCard from '../components/ExpenseCard';
 
 const Split = () =>{
     const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
+    const outlet = useOutlet();//To show expense list when the page is in index.
     var { splitId } = useParams();
     var split: SplitModel|undefined = useTypedSelector(state => state.splits.find(sp => sp.id === splitId));
+    var expenses: Expense[] = useTypedSelector(state => state.expenses);
     return(
         <>
             {split && <div className="split-page main-content-area">
@@ -19,7 +23,9 @@ const Split = () =>{
                         <div className="header-split-name">{split.name}</div>
                     </div>
                 </div>
-                <Outlet />
+                {outlet || <div className="split-expense-list-cont">
+                    {expenses.map(expense => <ExpenseCard model={expense}/>)}
+                </div>}
             </div>}
         </>
     )
