19cc970b068c2b7f9e2c37688dd1560dd8d3be50
Thaya <selvathayalan.r@gmail.com>
2023-07-14 23:38:27 +0530

    Implemented showing the split overview details for overall and the split wise

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,19 +3,68 @@ import { useSelector, TypedUseSelectorHook } from "react-redux";
 import { Split } from "../models/Split";
 import { useNavigate } from "react-router-dom";
 import { RootState } from '../store/store';
+import { SplitOverview } from '../models/SplitOverview';
+import { Member } from '../models/Member';
 
 const Home = () => {
     const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
     const navigate = useNavigate();
     var splitList: Split[] = useTypedSelector(state => state.splits);
+    var userAccount: Member = useTypedSelector(state => state.userAccount);
 
     const navToSplitPage = (splitId?: string) => {
         navigate(`/split/${splitId}`)
     }
 
+    function getSplitOverviewDetails(overview: SplitOverview[]){
+        let elements = [];
+        let totalBorrow = 0, totalLend = 0;
+        let userId = userAccount.id;
+        overview.forEach(item => {
+            if(item.memberId == userId){
+                totalLend += item.amount;
+            }
+            else if(item.owesId == userId){
+                totalBorrow += item.amount;
+            }
+        });
+        if(totalLend > 0){
+            elements.push(<li>You lend <b>{totalLend}</b></li>);
+        }
+        if(totalBorrow > 0){
+            elements.push(<li>You owe <b>{totalBorrow}</b></li>);
+        }
+        return elements;
+    }
+
+    function getOverAllDetails(splits: Split[]){
+        let elements = [];
+        let totalBorrow = 0, totalLend = 0;
+        let userId = userAccount.id;
+        splits.forEach(split => {
+            split.overviews.forEach(item => {
+                if(item.memberId == userId){
+                    totalLend += item.amount;
+                }
+                else if(item.owesId == userId){
+                    totalBorrow += item.amount;
+                }
+            });
+        });
+        if(totalLend > 0){
+            elements.push(<li>You lend <b>{totalLend}</b></li>);
+        }
+        if(totalBorrow > 0){
+            elements.push(<li>You owe <b>{totalBorrow}</b></li>);
+        }
+        return elements;
+    }
+
     return(
         <div className="home-page main-content-area">
-            <div className="total-split-overview">{}</div>
+            <ul className="total-split-overview no-list-style">
+                {getOverAllDetails(splitList)}
+            </ul>
             <ul className="split-list no-list-style">
                 {splitList.map(split => 
                     <li className="split-cont" onClick={() => navToSplitPage(split.id)}>
@@ -24,7 +73,9 @@ const Home = () => {
                         </div>
                         <div className="split-details">
                             <div className="split-name">{split.name}</div>
-                            <div className="split-overview"></div>
+                            <ul className="split-overview no-list-style">
+                                {getSplitOverviewDetails(split.overviews)}
+                            </ul>
                         </div>
                     </li>)}
             </ul>
