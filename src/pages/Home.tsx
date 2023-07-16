31ff965bcbce19443168fc9b0a9c56569d5dbb9e
Thaya <selvathayalan.r@gmail.com>
2023-07-16 11:07:13 +0530

    Implemented the split detail overview for different scenarios

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,9 @@ import { useNavigate } from "react-router-dom";
 import { RootState } from '../store/store';
 import { SplitOverview } from '../models/SplitOverview';
 import { Member } from '../models/Member';
+import { getMemeberNameById } from '../utils/Common';
+
+const MAX_DETAILED_LIST_COUNT = 3;
 
 const Home = () => {
     const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
@@ -16,23 +19,79 @@ const Home = () => {
         navigate(`/split/${splitId}`)
     }
 
-    function getSplitOverviewDetails(overview: SplitOverview[]){
+    function getSplitOverviewDetails(overview: SplitOverview[], members: Member[]){
         let elements = [];
-        let totalBorrow = 0, totalLend = 0;
+        let totalBorrow = 0, totalLend = 0, totalBorrowersCount = 0, totalLendersCount = 0;
         let userId = userAccount.id;
         overview.forEach(item => {
-            if(item.memberId == userId){
+            if(item.memberId === userId){
                 totalLend += item.amount;
+                totalLendersCount++;
             }
-            else if(item.owesId == userId){
+            else if(item.owesId === userId){
                 totalBorrow += item.amount;
+                totalBorrowersCount++;
             }
         });
         if(totalLend > 0){
-            elements.push(<li>You lend <b>{totalLend}</b></li>);
+            if(totalLendersCount > 1){
+                elements.push(<li className="positive-color">You lend <b>{totalLend}</b></li>);
+                let lendDetails = [], count = 0;
+                for(let i=0; i<overview.length; i++){
+                    let item = overview[i];
+                    if(item.memberId === userId){
+                        count++;
+                        if(count < MAX_DETAILED_LIST_COUNT || count === totalLendersCount){
+                            lendDetails.push(<p>{getMemeberNameById(members, item.owesId)} owes you {item.amount}</p>);
+                        }
+                        else{
+                            lendDetails.push(<p>Plus <b>{totalLendersCount - lendDetails.length} Members</b> owes you</p>);
+                            break;
+                        }
+                    }
+                }
+                elements.push(<li className="split-overview-details">{lendDetails}</li>);
+            }
+            else{
+                for(let i=0; i<overview.length; i++){
+                    let item = overview[i];
+                    if(item.memberId === userId){
+                        elements.push(<li className="positive-color">{getMemeberNameById(members, item.owesId)} owes you <b>{item.amount}</b></li>);
+                        break;
+                    }
+                }
+            }
         }
         if(totalBorrow > 0){
-            elements.push(<li>You owe <b>{totalBorrow}</b></li>);
+            if(totalBorrowersCount > 1){
+                elements.push(<li className="negative-color">You owe <b>{totalBorrow}</b></li>);
+                if(totalLendersCount <= 1){
+                    let borrowDetails = [], count = 0;
+                    for(let i=0; i<overview.length; i++){
+                        let item = overview[i];
+                        if(item.owesId === userId){
+                            count++;
+                            if(count < MAX_DETAILED_LIST_COUNT || count === totalBorrowersCount){
+                                borrowDetails.push(<p>You owe {getMemeberNameById(members, item.memberId)} {item.amount}</p>);
+                            }
+                            else{
+                                borrowDetails.push(<p>You owe <b>{totalBorrowersCount - borrowDetails.length} Members</b> more</p>);
+                                break;
+                            }
+                        }
+                    }
+                    elements.push(<li className="split-overview-details">{borrowDetails}</li>);
+                }
+            }
+            else{
+                for(let i=0; i<overview.length; i++){
+                    let item = overview[i];
+                    if(item.owesId === userId){
+                        elements.push(<li className="negative-color">You owe {getMemeberNameById(members, item.memberId)} <b>{item.amount}</b></li>);
+                        break;
+                    }
+                }
+            }
         }
         return elements;
     }
@@ -68,14 +127,14 @@ const Home = () => {
             <ul className="split-list no-list-style">
                 {splitList.map(split => 
                     <li className="split-cont" onClick={() => navToSplitPage(split.id)}>
-                        <div className="split-pic">
+                        <div className="split-pic disp_flex">
                             <p>{split.name.charAt(0)}</p>
                         </div>
                         <div className="split-details">
                             <div className="split-name">{split.name}</div>
-                            <ul className="split-overview no-list-style">
-                                {getSplitOverviewDetails(split.overviews)}
-                            </ul>
+                            <div className="split-overview">
+                                {getSplitOverviewDetails(split.overviews, split.members)}
+                            </div>
                         </div>
                     </li>)}
             </ul>
--- src/store/reducers/splitsReducer.ts
+++ src/store/reducers/splitsReducer.ts
@@ -5,7 +5,7 @@ import ActionModel from "../models/ActionModel";
 const splitsInitialState: Split[] = [
     {id:"g0",name:"Chennai Life",type:SplitType.Group, 
         members: [{firstName: "Selva",lastName: "thayalan",id: "m0",emailId: "selvathayalan.r@gmail.com",mobile: "1234567890"},{id:"m1", lastName: "raj", firstName: "Mohan", mobile: "2345678901", emailId:"mohan@xyz.com"},{id:"m2", lastName: "", firstName: "Mariappan", mobile: "3456789012", emailId:"mari@xyz.com"}], 
-        overviews: [{memberId:"m0", owesId:"m1", amount: 10}, {memberId:"m0", owesId:"m2", amount: 20}, {memberId:"m2", owesId:"m1", amount: 20}]},
+        overviews: [{memberId:"m0", owesId:"m1", amount: 10}, {memberId:"m0", owesId:"m2", amount: 20}, {memberId:"m0", owesId:"m2", amount: 20}, {memberId:"m0", owesId:"m2", amount: 20}, {memberId:"m2", owesId:"m0", amount: 20}]},
     {id:"g1",name:"Mohanraj",type:SplitType.Individual, members: [], overviews: []},
     {id:"g2",name:"Pichakara pasanga",type:SplitType.Group, members: [], overviews: []},
 ]
