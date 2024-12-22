9ae38895a5fa8ef7bef48c432e59a5a0aff60650
Thaya <selvathayalan.r@gmail.com>
2024-12-22 16:38:54 +0530

    implemented global header #91726164

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './index.css';
 import './styles/Common.scss';
+import './styles/Responsive.scss';
 import Home from './pages/Home';
 import Split from './pages/Split';
 import AddExpense from './pages/expense/AddExpense';
@@ -10,6 +10,7 @@ import EditExpense from './pages/expense/EditExpense';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import CreateGroup from './pages/group/CreateGroup';
+import MainHeader from './components/MainHeader';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -17,16 +18,19 @@ const root = ReactDOM.createRoot(
 root.render(
   <Provider store={store}>
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/split/:splitId' element={<Split/>}>
-          <Route path='add' element={<AddExpense/>} />
-          <Route path='edit/:expenseId' element={<EditExpense/>} />
-        </Route>
-        <Route path='group'>
-          <Route path='new' element={<CreateGroup/>} />
-        </Route>
-      </Routes>
+      <MainHeader/>
+      <body className="main-content">
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/split/:splitId' element={<Split/>}>
+            <Route path='add' element={<AddExpense/>} />
+            <Route path='edit/:expenseId' element={<EditExpense/>} />
+          </Route>
+          <Route path='group'>
+            <Route path='new' element={<CreateGroup/>} />
+          </Route>
+        </Routes>
+      </body>
     </BrowserRouter>
   </Provider>
 );
